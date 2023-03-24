4add8fbb79e35be7317e30bf39d1a451b3d9520e
bribrie <ibriebriee@gmail.com>
2023-03-24 18:20:16 +0900

    tutorial: path operation configuration

--- _routers/item.py
+++ _routers/item.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query, Path, Body, HTTPException
+from fastapi import APIRouter, Query, Path, Body, HTTPException, status
 from typing import Union, Annotated
 from _models.item import Item, OmitItem
 from _models.user import User
@@ -8,8 +8,26 @@ router = APIRouter()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 # Declare data model as a parameter
-@router.post("/items/", response_model=Item, status_code=201)
+# Tags, Summary, Status code, Response description
+# Description from docstring - can write Markdown in the docstring
+@router.post(
+    "/items/", 
+    response_model=Item, 
+    status_code=status.HTTP_201_CREATED, 
+    tags=["items"], 
+    summary="Create an item",
+    response_description="The created item"    
+)
 async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
     return item
     # item_dict = item.dict()
     # if item.tax:
@@ -18,10 +36,15 @@ async def create_item(item: Item):
     # return item_dict
 
 # Multiple body parameters
-@router.put("/items/{item_id}")
+@router.put("/items/{item_id}", tags=["items"])
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)], user: User):
     results = {"item_id":item_id, "item": item, "user": user}
     return results
+
+# Deprecate a path operation
+@router.get("/deitems/", tags=["items"], deprecated=True)
+async def read_items():
+    return [{"item_id": "Foo"}]
 
 # Request body + path + query parameters
 # @router.put("/items/{item_id}")
@@ -39,7 +62,7 @@ async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)], use
     #     result.update({"q": q})
     # return result 
 
-@router.get("/items/")
+@router.get("/items/", tags=["items"])
 # async def read_items(q: Annotated[Union[str, None], Query(min_length=3, max_length=50, regex="^fixedquery$")] = None):
 # async def read_items(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
 async def read_items(q: str):
